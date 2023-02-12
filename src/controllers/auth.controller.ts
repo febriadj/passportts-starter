b9a626dbd81bceaaf23ac3956f569cfc1b9a658b
febriadj <iamfebriadji@gmail.com>
2023-02-12 22:19:09 +0700

    feat(api): add github auth route

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction as Next } from 'express';
 import passport from 'passport';
 import generateToken from '../helpers/generate-token.helper';
 import * as httpRes from '../helpers/http-response.helper';
+import config from '../config';
 
 type TController = (req: Request, res: Response, next?: Next) => Promise<void>;
 
@@ -34,6 +35,36 @@ export const local: TController = async (req, res, next) => {
         message: info.message,
         payload: user,
       });
+    }
+  )(req, res, next);
+};
+
+/**
+ * Router controller for OAuth authentication callback.
+ * @param {Request} req
+ * @param {Response} res
+ * @param {Next} next
+ */
+export const oauth: TController = async (req, res, next) => {
+  const { originalUrl: url } = req;
+  const strategy: string = url.split('/').reverse()[1];
+
+  const successUrl = `${config.clientHost}${config.oauth.successRedirect}`;
+  const failureUrl = `${config.clientHost}${config.oauth.failureRedirect}`;
+
+  passport.authenticate(
+    strategy,
+    { session: false },
+    async (err, user): Promise<void> => {
+      if (err) {
+        res.redirect(failureUrl);
+        return;
+      }
+
+      const token: string = await generateToken(user._id);
+      // set cookie.
+      res.cookie('x-auth-cookie', token);
+      res.redirect(successUrl);
     }
   )(req, res, next);
 };
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,12 @@
 import { Router, IRouter } from 'express';
+import passport from 'passport';
 import * as auth from '../controllers/auth.controller';
 
 const router: IRouter = Router();
 
 router.post('/auth/local', auth.local);
+
+router.get('/auth/github', passport.authenticate('github'));
+router.get('/auth/github/callback', auth.oauth);
 
 export default router;
