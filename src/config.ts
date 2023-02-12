f1a2e2b7c3f2883f6d1cacd7d671b93fc43d8f4f
febriadj <iamfebriadji@gmail.com>
2023-02-12 19:54:56 +0700

    chore(config): add clientHost and oauth options

--- src/config.ts
+++ src/config.ts
@@ -1,29 +1,43 @@
 import { CorsOptions } from 'cors';
 import { ConnectOptions } from 'mongoose';
 
-interface IDatabase {
+export interface IDatabase {
   uri: string;
   options?: ConnectOptions;
 }
 
-interface IConfig {
+export interface IOAuthOptions {
+  successRedirect: string;
+  failureRedirect: string;
+}
+
+export interface IConfig {
   isProd: boolean;
   port: number;
+  clientHost: string;
   cors: CorsOptions;
   db: IDatabase;
+  oauth: IOAuthOptions;
 }
+
+const clientHost = 'http://localhost:3000';
 
 const config: Readonly<IConfig> = {
   isProd: process.env.NODE_ENV === 'production',
   port: 8080,
+  clientHost,
   cors: {
-    origin: ['http://localhost:3000'],
+    origin: [clientHost],
     // optionsSuccessStatus: 204,
     // methods: '',
   },
   db: {
     uri: 'mongodb://127.0.0.1:27017/passportts-starter',
     // options: {},
+  },
+  oauth: {
+    successRedirect: '/dashboard',
+    failureRedirect: '/login',
   },
 };
 
