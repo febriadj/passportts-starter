2affa7940d9bdf80054f8505155f9a785e1bafc5
febriadj <iamfebriadji@gmail.com>
2023-02-13 21:05:24 +0700

    feat(api): add google auth route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -6,7 +6,15 @@ const router: IRouter = Router();
 
 router.post('/auth/local', auth.local);
 
+// github authentication.
 router.get('/auth/github', passport.authenticate('github'));
 router.get('/auth/github/callback', auth.oauth);
+
+// google authentication.
+router.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+router.get('/auth/google/callback', auth.oauth);
 
 export default router;
